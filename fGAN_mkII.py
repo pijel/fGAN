595104188e9dc0c9dca89be1f391985d5fad0778
Senthil Rajasekaran <srajasek93@gmail.com>
2018-07-26 03:29:29 -0500

    updated symmetric training and added MKII
    -the code is more symmetric, more than likely fixing bugs of ommision
    
    -an attempt to make  training in MKII nonsymmetric by setting the learning rate to 0
    
    -cleaned up code/lines in general, cataching a couple mistakes
    
    -generator now visibly does something for  both files, which can be sometimes observed for even small epochs/iterations

--- fGAN_mkII.py
+++ fGAN_mkII.py
@@ -76,7 +76,7 @@ minibatch_size = d_input_size
 d_learning_rate = 2e-4  # 2e-4
 g_learning_rate = 2e-4
 optim_betas = (0.9, 0.999)
-num_epochs = 200
+num_epochs = 400
 print_interval = 10
 d_steps = 1  # 'k' steps in the original GAN paper. Can put the discriminator on higher training freq than generator
 g_steps = 1
@@ -146,6 +146,7 @@ D_queue.add(first_discrim)
 G_queue.add(first_gen)
 
 
+
 #It might be the case that the gaussian distribution is changing everytime with new calls of the get_distribution_sampler function,
 #which is decidedly bad. 
 #Never mind, it seems that it's initialized to the same thing every time. It could be useful to just store it memory to save computing time?
@@ -166,14 +167,55 @@ for i in range(10):
         #take the top element of the queue. Now, do just the discrimator training from as given in the medium article code
 
 
-        d_sampler = get_distribution_sampler(data_mean, data_stddev)
         gi_sampler = get_generator_input_sampler()
+        d_sampler = get_distribution_sampler(data_mean, data_stddev)
         #G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)
         #D = Discriminator(input_size=d_input_func(d_input_size), hidden_size=d_hidden_size, output_size=d_output_size)
         criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
         d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas)
-        g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
+        g_optimizer = optim.Adam(G.parameters(), lr= 0, betas=optim_betas)
+        #the learning rate here is 0, we don't want G to train. I know there is no code here but it is for symmetry purposes
 
+        for epoch in range(num_epochs):
+            for d_index in range(d_steps):
+                # 1. Train D on real+fake
+                D.zero_grad()
+
+                #  1A: Train D on real
+                d_real_data = Variable(d_sampler(d_input_size))
+                d_real_decision = D(preprocess(d_real_data))
+                d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
+                d_real_error.backward() # compute/store gradients, but don't change params
+
+                #  1B: Train D on fake
+                d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+                d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
+                d_fake_decision = D(preprocess(d_fake_data.t()))
+                d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
+                d_fake_error.backward()
+                d_optimizer.step()     # Only optimizes D's parameters; changes based on stored gradients from backward()
+        #print("REACHED")
+        G_queue.add(G)
+        #add the element to the end of the queue so the structure is preserved. Note that due to the fixed length of the queue, this command will implicitly remove
+        #elements as we go to the queue_size variable
+
+
+    D_queue.add(D)
+    #add the newly trained element back into the discrimator queue
+
+    #the modifications made below are largely the same as above. Maybe it's a good idea to also modify the print statement below, in order to get more relevant metrics 
+    G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)   
+    D_size = D_queue.size
+
+    for i in range(D_size):
+
+        D = D_queue.remove()
+        gi_sampler = get_generator_input_sampler()
+        d_sampler = get_distribution_sampler(data_mean, data_stddev)
+        criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
+        d_optimizer = optim.Adam(D.parameters(), lr=0, betas=optim_betas)
+        #once again, learning rate is 0. We don't want D to train.
+        g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
         for epoch in range(num_epochs):
             for d_index in range(d_steps):
                 # 1. Train D on real+fake
@@ -192,61 +234,6 @@ for i in range(10):
                 d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
                 d_fake_error.backward()
                 d_optimizer.step()
-            
-            for g_index in range(g_steps):
-            # 2. Train G on D's response (but DO NOT train D on these labels)
-                G.zero_grad()
-
-                gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
-                g_fake_data = G(gen_input)
-                dg_fake_decision = D(preprocess(g_fake_data.t()))
-                g_error = criterion(dg_fake_decision, Variable(torch.ones(1)))  # we want to fool, so pretend it's all genuine
-
-                g_error.backward()
-                g_optimizer.step()  # Only optimizes G's parameters
-        
-        # Only optimizes D's parameters; changes based on stored gradients from backward()
-        #print("REACHED")
-        G_queue.add(G)
-        #add the element to the end of the queue so the structure is preserved. Note that due to the fixed length of the queue, this command will implicitly remove
-        #elements as we go to the queue_size variable
-
-
-    D_queue.add(D)
-    #add the newly trained element back into the discrimator queue
-
-    #the modifications made below are largely the same as above. Maybe it's a good idea to also modify the print statement below, in order to get more relevant metrics 
-    G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)   
-    D_size = D_queue.size
-
-    for i in range(D_size):
-        
-
-        D = D_queue.remove()
-        d_sampler = get_distribution_sampler(data_mean, data_stddev)
-        gi_sampler = get_generator_input_sampler()
-        #G = Generator(input_size=g_input_size, hidden_size=g_hidden_size, output_size=g_output_size)
-        #D = Discriminator(input_size=d_input_func(d_input_size), hidden_size=d_hidden_size, output_size=d_output_size)
-        criterion = nn.BCELoss()  # Binary cross entropy: http://pytorch.org/docs/nn.html#bceloss
-        d_optimizer = optim.Adam(D.parameters(), lr=d_learning_rate, betas=optim_betas)
-        g_optimizer = optim.Adam(G.parameters(), lr=g_learning_rate, betas=optim_betas)
-        for epoch in range(num_epochs):
-            for d_index in range(d_steps):
-                # 1. Train D on real+fake
-                D.zero_grad()
-
-                #  1A: Train D on real
-                d_real_data = Variable(d_sampler(d_input_size))
-                d_real_decision = D(preprocess(d_real_data))
-                d_real_error = criterion(d_real_decision, Variable(torch.ones(1)))  # ones = true
-                d_real_error.backward() # compute/store gradients, but don't change params
-
-                #  1B: Train D on fake
-                d_gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
-                d_fake_data = G(d_gen_input).detach()  # detach to avoid training G on these labels
-                d_fake_decision = D(preprocess(d_fake_data.t()))
-                d_fake_error = criterion(d_fake_decision, Variable(torch.zeros(1)))  # zeros = fake
-                d_fake_error.backward()
 
             for g_index in range(g_steps):
             # 2. Train G on D's response (but DO NOT train D on these labels)
