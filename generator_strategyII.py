3c6201ce8a6d6f6e1fa736a8db7a6ec9cceaaa0d
Senthil Rajasekaran <srajasek93@gmail.com>
2018-09-09 14:27:10 -0500

    new strategy
    fixed so that it only generates with each generator once, as opposed to the other strategy which will generate every time a data point is needed.

---
+++ generator_strategyII.py
@@ -0,0 +1,27 @@
+import random
+def generator_strategy(gen_array,output_size):
+    #this time we are passed an array of generators
+    #the idea will be that they generate once, and then we sample
+    
+    #this is when they generate
+    
+    fake_data_array = []
+    gen_input = Variable(gi_sampler(minibatch_size, g_input_size))
+    for i in gen_array:
+        i.zero_grad()
+        new_data = i(gen_input)
+        fake_data_array.append(new_data)
+        
+    #fake_data_array should contain all generator's fake data. now, we need to sample
+    out = []
+    for i in range(output_size):
+        
+        #choose a random generator
+        
+        generator_choice = random.randint(0,len(gen_array)-1)
+        index = random.randint(0,output_size - 1)
+        
+        out.append(fake_data_array[generator_choice][index])
+    
+    #!!! dim is only 0 for this 1d mog !!!
+    return torch.cat(out,dim = 0)
